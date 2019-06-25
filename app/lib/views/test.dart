fcd783c96618622edef0413d0f7361fdcc6e4b77
ngoan98tv <ngoan98travinh@gmail.com>
2019-06-25 21:53:33 +0700

    fix bugs in test

--- app/lib/views/test.dart
+++ app/lib/views/test.dart
@@ -12,49 +12,36 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test',
-      home: Scaffold(
-        body: FutureBuilder(
-          future: loadMajors(),
-          builder: (context, snapshot){
-            if (snapshot.connectionState == ConnectionState.done)
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index){
-                  return ListTile(
-                    title: Text(snapshot.data[index].majorName),
-                    subtitle: Column(
-                      children: <Widget>[
-                        Text("Ma nganh: ${snapshot.data[index].majorCode}"),
-                        Text("Video: ${snapshot.data[index].videoURL}"),
-                        Text("Thoi gian dao tao: ${snapshot.data[index].intendTime}"),
-                        Text("Danh hieu: ${snapshot.data[index].title}"),
-                        Text("Khoa: ${snapshot.data[index].collegeName}"),
-                        Text("Anh minh hoa: ${snapshot.data[index].collegeImage}"),
-                        Text("Nhom nganh: ${snapshot.data[index].majorGroup}"),
-                        Text("Phuong thuc tuyen sinh: ${snapshot.data[index].inputRequest[0].testMethod}"),
-                        ListView.builder(
-                          itemCount: snapshot.data[index].inputRequest[0].combinations.length,
-                          itemBuilder: (context, index){
-                            return ListTile(
-                              subtitle: Column(
-                                children: <Widget>[
-                                  Text("${snapshot.data[index].inputRequest[0]..combinations[index]}")
-                                ],
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    ),
-                  );
-                },
-              );
-              return CircularProgressIndicator();
-          },
-          ),
-      )
+    return Scaffold(
+      body: FutureBuilder(
+        future: loadMajors(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            List<Major> majors = snapshot.data;
+            return ListView.builder(
+              itemCount: majors.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: <Widget>[
+                      Text(majors[index].majorName),
+                      Text("Ma nganh: ${majors[index].majorCode}"),
+                      Text("Video: ${majors[index].videoURL}"),
+                      Text("Thoi gian dao tao: ${majors[index].intendTime}"),
+                      Text("Danh hieu: ${majors[index].title}"),
+                      Text("Khoa: ${majors[index].collegeName}"),
+                      Text("Anh minh hoa: ${majors[index].collegeImage}"),
+                      Text("Nhom nganh: ${majors[index].majorGroup}"),
+                      Text("Phuong thuc tuyen sinh: ${majors[index].inputRequest[0].testMethod}"),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 
@@ -68,5 +55,4 @@ class _TestState extends State<Test> {
     final jsonResponse = json.decode(jsonString);
     return majorsList(jsonResponse);
   }
-
 }
