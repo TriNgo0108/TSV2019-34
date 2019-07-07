3967981e94a5afdfdb6be1b188e35cbfdd65e248
calavori <calavori1998@gmail.com>
2019-07-07 10:01:41 +0700

    update test

--- app/lib/views/test.dart
+++ app/lib/views/test.dart
@@ -17,13 +17,31 @@ class _TestState extends State<Test> {
           if (snapshot.connectionState == ConnectionState.done) {
             List<Major> majors = snapshot.data;
             return GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
               itemCount: majors.length,
               itemBuilder: (context, index) {
                 return Card(
                   child: Column(
                     children: <Widget>[
                       Text(majors[index].majorName),
+                      // Text(majors[index].majorCode),
+                      // Text(majors[index].intendTime),
+                      // Text(majors[index].title),
+                      // Text(majors[index].collegeName),
+                      // Text(majors[index].collegeImage),
+                      // Text(majors[index].inputRequest[0].testMethod),
+                      // Text(majors[index].inputRequest[0].combinations[0]),
+                      // Text("${majors[index].inputRequest[0].quantity}"),
+                      // Text(majors[index].mainSubjects[0]),
+                      // Text(majors[index].majorGroup),
+                      // Text("${majors[index].seasons[0].year}"),
+                      // Text("${majors[index].seasons[0].points}"),
+                      Text("${majors[index].specializations[0].specializationName}"),
+                      Text("${majors[index].specializations[0].videoURL}"),
+                      Text("${majors[index].specializations[0].objectives[0]}"),
+                      Text("${majors[index].specializations[0].jobs[0]}"),
+                      Text("${majors[index].specializations[0].developAbilities[0]}"),
+                      Text("${majors[index].specializations[0].standardOutput}"),
                     ],
                   ),
                 );
