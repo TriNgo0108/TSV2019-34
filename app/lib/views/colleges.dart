0105dd39cdfb2ec374e88a9330a66f9a68e1ba8d
ngoan98tv <ngoan98travinh@gmail.com>
2019-09-28 16:33:48 +0700

    update colleges info

---
+++ app/lib/views/colleges.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Colleges extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Danh sách các khoa viện"),
+      ),
+      body: Column(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
